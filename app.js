df9e68c1e1bad41650af06b5e418e8f5b6d9602e
Ido Kleinman <idokleinman@gmail.com>
2018-03-13 02:56:32 -0700

    add MiningHamster

--- app.js
+++ app.js
@@ -2,9 +2,29 @@
 
 require('dotenv').config();
 const BittrexController = require('./lib/BittrexController');
+const MiningHamsterController = require('./lib/MiningHamsterController');
 
 console.log('welcome');
 
 var bc = new BittrexController();
-bc.getmarketsummaries();
-bc.getcandles('BTC-ETH','fiveMin');
+var mh = new MiningHamsterController();
+
+bc.getMarketSummaries();
+// bc.getCandles('BTC-ETH','fiveMin');
+
+
+mh.getTestSignal();
+
+function loopGetSignals() {
+	// do whatever you like here
+	console.log('---');
+
+	mh.getSignal();
+	setTimeout(loopGetSignals, 2000);
+}
+
+loopGetSignals();
+
+
+
+
