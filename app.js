8f8f8f75562731d1b686139cb17a1d2c0a4dbb2f
Ido Kleinman <idokleinman@gmail.com>
2018-03-13 01:28:57 -0700

    refactor bittrex api to a class module

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
 'use strict';
 
 require('dotenv').config();
-let bittrex = require('node-bittrex-api');
-
-bittrex.options({
-  'apikey' : process.env.BITTREX_API_KEY,
-  'apisecret' : process.env.BITTREX_API_SECRET,
-});
-
-
-bittrex.getmarketsummaries( function( data, err ) {
-  if (err) {
-    return console.error(err);
-  }
-  for( var i in data.result ) {
-    bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
-      console.log( ticker );
-    });
-  }
-});
+const BittrexController = require('./lib/BittrexController');
 
 console.log('welcome');
-console.log(process.env.BITTREX_API_KEY);
+var bc = new BittrexController();
+bc.getmarketsummaries();
