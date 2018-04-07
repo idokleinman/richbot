e5a993b4722e64116d2094bfea55a054b71b1734
Ido Kleinman <idokleinman@gmail.com>
2018-04-08 02:59:12 +0300

    add advanced logging + file (and retries)

--- app.js
+++ app.js
@@ -4,16 +4,16 @@
 // const BittrexController = require('./lib/BittrexController');
 const MiningHamsterController = require('./lib/MiningHamsterController');
 const PositionsManager = require('./lib/PositionsManager');
+// const DatabaseManager = require('./lib/DatabaseManager');
 const settings = require('./settings');
 const death = require('death');
 const stringHash = require('string-hash');
 // const _ = require('lodash');
+const logger = require('./lib/utils/logger');
 
 // var bc = new BittrexController();
 var mh = new MiningHamsterController();
 var pm = new PositionsManager();
-// bc.getMarketSummaries();
-// bc.getCandles('BTC-ETH','fiveMin');
 
 var lastSignalHash = 0;
 
@@ -26,35 +26,65 @@ death(function(signal, err) {
 });
 
 
+async function _enterPositionOnSignal(signal, retryCounter) {
+	if (retryCounter < 0) {
+		return Promise.reject('Out of retries');
+	}
+	let enterPosition = await pm.enter(signal).catch((result) => {
+
+		if (result.type === 'warn') {
+			logger.warn(result.msg);
+			return Promise.reject('Warning');
+		}
+
+		if (result.type === 'error') {
+			logger.error(result.msg);
+			setTimeout(function() {
+				_enterPositionOnSignal(signal, retryCounter-1)
+			}, 5000)
+		}
+
+	});
+
+	if (enterPosition) {
+		logger.info(`Create new position for signal success`);
+		return Promose.resolve(true);
+	}
+
+
+}
+
 
 async function loopGetSignals() {
 
 	let signals = await mh.getSignal().catch(error => {
-		console.log(`~ Failed getting signal from MiningHamster - ${error}`);
+		logger.warn(`Failed getting signal from MiningHamster - ${error}`);
 	});
 
 	if (signals) {
 		let signal = signals[0];
 		// calculate time since signal
 		let timeSinceSignal = mh.timeSinceSignal(signal);
+
+		logger.debug(timeSinceSignal);
 		// process.stdout.write(`.`);
 
 		// debug time check disabled:
-		if ((timeSinceSignal < settings.trade.max_signal_time_diff_to_buy) && (timeSinceSignal > settings.trade.min_signal_time_diff_to_buy)) {
+		if ((timeSinceSignal < settings.trade.max_signal_time_diff_to_buy) && (timeSinceSignal >= settings.trade.min_signal_time_diff_to_buy)) {
 		// if (true) {
 			// console.log('got signal:');
 			let signalHash = stringHash(signal.toString());
 
 			if (signalHash !== lastSignalHash) {
-				console.log(`- New signal published ${new Date()}`);
-				console.log(signal);
+				logger.debug(`New signal published ${JSON.stringify(signal)}`);
 				signal.time = mh.convertSignalTimeToIsoDate(signal);
-				let enterPosition = await pm.enter(signal).catch((error) => {
-					console.log(`${error}`);
+
+				let enterPosition = await _enterPositionOnSignal(signal, 5).catch((error) => {
+					// logger.warn(`Could not enter position because: ${error}`);
 				});
 
 				if (enterPosition) {
-					console.log(`- Enter position for new signal success at ${new Date()}`);
+					logger.info(`Enter position for new signal success`);
 				}
 
 				lastSignalHash = signalHash;
@@ -72,7 +102,7 @@ async function loopGetSignals() {
 	setTimeout(loopGetSignals, settings.mh_signals.polling_interval*1000);
 }
 
-console.log('* Welcome to RichBot! Let\'s make some auto-money...');
+logger.info('$$$ Welcome to RichBot! Let\'s make some money! $$$');
 loopGetSignals();
 
 // TODO:
@@ -80,6 +110,7 @@ loopGetSignals();
 // Binance API support - future
 // TA bot
 // check signal TA indicators before entering position (wait for MACD cross or RSI <30 etc) - same for exiting position
+// file logging / reports
 
 
 
